685a1491669b316313b0a2c40f22cfb76c22b308
Sunil Kumar <SunilKumar.Tatineni@Chubb.com>
2021-12-21 17:33:22 +0530

    first commit

---
+++ src/components/common/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { NavDropdown, Nav, Navbar, Container } from "react-bootstrap";
+
+const Header = () => {
+  const activeStyle = { color: "#F15B2A" };
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <nav>
+        <NavLink to="/" activeStyle={activeStyle} exact>
+          Home
+        </NavLink>
+        {" | "}
+        <NavLink to="/courses" activeStyle={activeStyle}>
+          Courses
+        </NavLink>
+        {" | "}
+        <NavLink to="/about" activeStyle={activeStyle}>
+          About
+        </NavLink>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
